efd05776ec56aa971722600739632f798dfd1f1d
Thorsten Deienrt <thorsten.deinert@udo.edu>
2022-09-20 15:01:40 +0200

    Add BinaryNode to represent binary trees. - Included ITreeNode interface to DRY formatting code

--- tree_data/Program.cs
+++ tree_data/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// See https://aka.ms/new-console-template for more information
+using BNode = Tree.Data.BinaryNode<string>;
+
+var root = new BNode("Root");
+var a = new BNode("A");
+var b = new BNode("B");
+var c = new BNode("C");
+var d = new BNode("D");
+var e = new BNode("E");
+var f = new BNode("F");
+
+root
+  .AddLeft(
+    a
+    .AddLeft(c)
+    .AddRight(d))
+  .AddRight(
+    b.AddRight(
+      e.AddLeft(f))
+  );
+
+var nodes = new[] { root, a, b, c, d, e, f };
+foreach (var node in nodes)
+{
+  Console.WriteLine(node);
+}
