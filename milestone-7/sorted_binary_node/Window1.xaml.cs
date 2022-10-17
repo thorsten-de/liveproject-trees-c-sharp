bbea20d9e57215c05c68a99e4ea03628ad983a33
Thorsten Deienrt <thorsten.deinert@udo.edu>
2022-10-17 15:54:13 +0200

    initial project from template

---
+++ milestone-7/sorted_binary_node/Window1.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Diagnostics;
+
+namespace sorted_binary_node1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void findButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void removeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void resetButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
